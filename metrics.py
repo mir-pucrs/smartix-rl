03ee0e5f8a78226c5a0845d2108fae0b89c51cf8
Priscilla <priscillaneuhaus@gpin.portoalegre.pucrsnet.br>
2018-09-14 16:31:31 -0300

    more functions

--- metrics.py
+++ metrics.py
@@ -1,5 +1,6 @@
-import throughput
-import power_size
+from throughput import Throughput
+from power_size import PowerSize
+from environment import Environment
 
 
 class Metrics:
@@ -8,35 +9,41 @@ class Metrics:
         self.temp_val = 0
         self.geoMean = 0
         self.powerSize = 0
+        self.env = Environment()
+        self.tput = Throughput()
+        self.powerSize = PowerSize()
 
+    def calc_throughput(self):
+        stream = 2
+        global temp_val
+        res_time_execution = self.tput.comp_time_execution()
+        temp_val = sum(res_time_execution.values())
+        print("Tempo total de execução: ", temp_val)
+        total = ((stream * 2) / temp_val) * 3600 * 1
+        print("Throughput ", total)
 
-def calc_throughput():
-    stream = 2
-    global temp_val
-    res_time_execution = throughput.comp_time_execution()
-    temp_val = sum(res_time_execution.values())
-    print("Tempo total de execução: ", temp_val)
-    total = ((stream * 2) / temp_val) * 3600 * 1
-    print("Throughput ", total)
+    def get_current_state(self):
+        current_state = self.env.get_indexes()
+        print(current_state)
 
+    def calc_power_size(self):
+        global geoMean
+        global powerSize
+        time_power_size = self.powerSize.comp_time_power_size()
+        count = time_power_size[1]
+        total_time = time_power_size[1]
+        geoMean = (total_time**(1/count))
+        powerSize = (3600/geoMean) * 1
+        print("\nTotal: ", total_time, "\nGeometric mean: ", geoMean, "\nSize: ", count, "\nPower Size: ", powerSize)
 
-def calc_power_size():
-    global geoMean
-    global powerSize
-    time_power_size = power_size.comp_time_power_size()
-    count = time_power_size[1]
-    total_time = time_power_size[1]
-    geoMean = (total_time**(1/count))
-    powerSize = (3600/geoMean) * 1
-    print("\nTotal: ", total_time, "\nGeometric mean: ", geoMean, "\nSize: ", count, "\nPower Size: ", powerSize)
-
-
-def main():
-    throughput.run_functions()
-    power_size.run_functions()
-    calc_throughput()
-    calc_power_size()
+    def run(self):
+        self.tput.run_functions()
+        self.powerSize.run_functions()
+        self.calc_throughput()
+        self.calc_power_size()
+        self.env.get_indexes()
+        self.env.build_env()
 
 
 if __name__ == '__main__':
-    main()
+    Metrics().run()
