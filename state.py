ffd2dd52af0572a1baeb93633572067de56b9358
Priscilla <priscillaneuhaus@gpin.portoalegre.pucrsnet.br>
2018-09-21 14:08:07 -0300

    refreshes

--- state.py
+++ state.py
@@ -19,16 +19,3 @@ class State:
         self.indexes = list(aux.values())
         print('indexes: ', self.indexes)
         return self.indexes
-
-    def add_index(self, column):
-        cnx = mysql.connector.connect(host='127.0.0.1', user='root', passwd='teste', db='tpch')
-        cursor = cnx.cursor(buffered=True)
-        for line in open('../name_of_indexes'):
-            command = ('CREATE INDEX %s ON lineitem (%s);\n' % (line.replace('\n', ''), column))
-            cursor.execute(command)
-
-    def drop_index(self, index):
-        cnx = mysql.connector.connect(host='127.0.0.1', user='root', passwd='teste', db='tpch')
-        cursor = cnx.cursor(buffered=True)
-        command = ('DROP INDEX %s ON lineitem;' % str(index))
-        cursor.execute(command)
