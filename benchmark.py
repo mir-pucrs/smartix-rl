a64344264a1808b84fb8c3ed3a3f2c89b7df1f38
Gabriel Paludo Licks <gabrielplicks@gmail.com>
2018-10-01 17:58:20 -0300

    Modified to choose benchmark in constructor

--- benchmark.py
+++ benchmark.py
@@ -3,26 +3,15 @@ from TPCH import TPCH
 
 class Benchmark:
 
-    benchmark = "TPCH"
 
-    def __init__(self):
-        pass
+    def __init__(self, benchmark = "TPCH"):
+        self.benchmark = benchmark
 
     def run(self, benchmark):
 
-        if (benchmark == "TPCH"):
+        if (self.benchmark == "TPCH"):
             results = TPCH().run()
         else:
             raise "No corresponding benchmark"
         
         return results
-
-
-'''
-Running TPCH and getting results, for example...
-
-if __name__ == '__main__':
-    results = Benchmark().run("TPCH")
-    print("Results:", results)
-
-'''
