9e434b5980165548a2992c7ef8e0ce4281d044fc
Gabriel Paludo Licks <gabrielplicks@gmail.com>
2018-10-02 16:12:47 -0300

    Added available_actions method

--- environment.py
+++ environment.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from state import State
+from action_repr import Action_repr
 
 class Environment:
 
@@ -10,7 +11,8 @@ class Environment:
     def execute(self, action):
         # if the agent is at state bla do bla
         indexes = self.state.get_map_indexes()
-
+        if action.type == 'DROP':
+            action.name
         """
         TODO: implement function to return state and state_reward
         """
@@ -24,3 +26,14 @@ class Environment:
         """
         TODO: implement function to compute rewards
         """
+
+    def available_actions(self, state):
+        list_actions = []
+        for index in state.map_indexes.keys():
+            a = Action_repr(index, 'DROP')
+            a1 = Action_repr(index, 'CREATE')
+            if state.map_indexes[index] == 0:
+                list_actions.append(a1)
+            else:
+                list_actions.append(a)
+        return list_actions
