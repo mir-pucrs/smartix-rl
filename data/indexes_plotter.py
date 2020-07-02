03e53ad55981260a89b1c3826b8be2eb2b7e2cd7
Gabriel Paludo Licks <gabrielplicks@gmail.com>
2020-07-02 20:39:45 -0300

    Plot scripts

--- data/indexes_plotter.py
+++ data/indexes_plotter.py
@@ -11,24 +11,18 @@ from pprint import pprint
 # Optimal indexes:
 # 12, 20, 22, 23, 28, 39
 
-outputs = [folder[0] for folder in os.walk('output/')][1:]
+folder = 'results/0.0001_0.9_100000_10000_128_1024_0.01_0.01_winsize40 (BEST)'
 
-for folder in outputs:
-    # with open(folder+'/rewards_history.json', 'r') as f:
-    #     rewards = json.load(f)
-    #     plt.title(folder)
-    #     plt.plot(rewards)
-    #     plt.show()
-    with open(folder+'/states_history.json', 'r') as f:
-        states = json.load(f)
-        states = states[:50000]
-        optimal = list()
-        total = list()
-        for state in states:
-            optimal.append(sum([state[12], state[20], state[22], state[23], state[28], state[39]]))
-            total.append(sum(state[:45]))
-        plt.title(folder)
-        plt.plot(optimal)
-        plt.plot(total)
-        plt.axhline(6, color='r', linestyle='--')
-        plt.show()
+with open(folder+'/states_history.json', 'r') as f:
+    states = json.load(f)
+    states = states[:50000]
+    optimal = list()
+    total = list()
+    for state in states:
+        optimal.append(sum([state[12], state[20], state[22], state[23], state[28], state[39]]))
+        total.append(sum(state[:45]))
+    plt.title(folder)
+    plt.plot(optimal)
+    plt.plot(total)
+    plt.axhline(6, color='r', linestyle='--')
+    plt.show()
--- data/stats_plotter.py
+++ data/stats_plotter.py
@@ -1,34 +1,12 @@
 import os
 import json
 import matplotlib.pyplot as plt
+import pandas as pd
 from pprint import pprint
 
-# Plots
-# 1 - Optimal indexes and total sum of indexes (with a ground truth line)
-# 2 - Reward history while training
-# 3 - Loss while training
+filename = 'results/0.0001_0.9_100000_10000_128_1024_0.01_0.01_winsize40 (BEST)/log.txt'
 
-# Optimal indexes:
-# 12, 20, 22, 23, 28, 39
+log = pd.read_table(filename, header=None, names=['episode', 'reward', 'loss', 'time', 'epsilon'])
 
-outputs = [folder[0] for folder in os.walk('output/')][1:]
-
-for folder in outputs:
-    # with open(folder+'/rewards_history.json', 'r') as f:
-    #     rewards = json.load(f)
-    #     plt.title(folder)
-    #     plt.plot(rewards)
-    #     plt.show()
-    with open(folder+'/states_history.json', 'r') as f:
-        states = json.load(f)
-        states = states[:50000]
-        optimal = list()
-        total = list()
-        for state in states:
-            optimal.append(sum([state[12], state[20], state[22], state[23], state[28], state[39]]))
-            total.append(sum(state[:45]))
-        plt.title(folder)
-        plt.plot(optimal)
-        plt.plot(total)
-        plt.axhline(6, color='r', linestyle='--')
-        plt.show()
+plt.plot(log['epsilon'])
+plt.show()
